cc84b21aa9bfacae4b8cc1f9991f12bfab0049c2
Andrew Popov <andrew.pp@darly.solutions>
2021-12-07 09:31:13 +0200

    fix letter headers

--- emailTemplates/en/base/baseHeader.blade.php
+++ emailTemplates/en/base/baseHeader.blade.php
@@ -10,13 +10,3 @@
         </mj-column>
     </mj-group>
 </mj-section>
-<mj-section css-class="phishing-content">
-    <mj-column>
-        <mj-text css-class="phishing-text">
-            {{empty($user) === false && $user->getSetting()->getAntiPhishingCode() ? "Anti-Phishing:" : "" }}
-            <span style="color: #FFFFFF">
-                {{empty($user) === false && $user->getSetting()->getAntiPhishingCode() ? $user->getSetting()->getAntiPhishingCode() : "" }}
-            </span>
-        </mj-text>
-    </mj-column>
-</mj-section>
